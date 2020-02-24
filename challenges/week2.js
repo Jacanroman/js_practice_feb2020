1e88c0e6fd661e54be6cf97ab419c7bf3e8ba28d
Jacanroman <jacanroman@gmail.com>
2020-02-24 11:46:20 +0000

    Challenges for week 2 done

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,55 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+    return sandwich.fillings
+
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.city === "Manchester"){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people/40);  //Math.ceil() round to up
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  sheepy =0
+  
+  /*
+  for (let i=0;i<arr.length;i++){
+    if(arr[i]==="sheep"){
+      sheepy++;
+    }
+  }
+  return sheepy
+  */
+
+  for (let i in arr){
+    if(arr[i]==="sheep"){
+      sheepy++;
+    }
+  }
+  return sheepy
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  // Your code here!ç
+  for (let k in person){
+    if (person.address.postCode.charAt(0).toUpperCase()==="M" && person.address.city.toUpperCase()=== "MANCHESTER"){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
 
 module.exports = {
